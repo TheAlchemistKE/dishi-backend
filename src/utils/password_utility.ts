89b7995784397bb6c98e198dde7aad8e6d38d73a
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-10 11:37:29 +0300

    change from snake_case to camelCase

--- src/utils/password_utility.ts
+++ src/utils/password_utility.ts
@@ -5,30 +5,30 @@ import { type VendorPayload } from '../dto'
 import { JWT_SECRET } from '../config'
 import { type AuthPayload } from '../dto/auth.dto'
 
-export const GenerateSalt = async () => {
+export const GenerateSalt = async (): Promise<string> => {
   return await bcrypt.genSalt()
 }
 
-export const GeneratePassword = async (password: string, salt: string) => {
+export const GeneratePassword = async (password: string, salt: string): Promise<string> => {
   return await bcrypt.hash(password, salt)
 }
 
 export const ValidatePassword = async (
-  entered_password: string,
-  saved_password: string | any,
+  enteredPassword: string,
+  savedPassword: string | any,
   salt: string | any
-) => {
-  return (await GeneratePassword(entered_password, salt)) === saved_password
+): Promise<boolean> => {
+  return (await GeneratePassword(enteredPassword, salt)) === savedPassword
 }
 
-export const GenerateSignature = async (payload: VendorPayload) => {
+export const GenerateSignature = async (payload: VendorPayload): Promise<string> => {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' })
 }
 
-export const ValidateSignature = async (req: Request) => {
+export const ValidateSignature = async (req: Request): Promise<boolean> => {
   const signature = req.get('Authorization') as string
-  if (signature) {
-    const payload = (await jwt.verify(
+  if (signature !== null) {
+    const payload = (jwt.verify(
       signature.split(' ')[1],
       JWT_SECRET
     )) as AuthPayload

365c6d3058ecd52529bc67f0d9ff26468ca8d75a
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-14 11:02:28 +0300

    create db reader and writer interfaces

---
+++ src/database/interfaces/reader.interface.ts
@@ -0,0 +1,10 @@
+import {Types} from 'mongoose'
+
+export interface PaginationOptions {
+    page?: number;
+    limit?: number;
+}
+export interface IReader<T> {
+	find(options?: PaginationOptions): Promise<T[]>
+	findOne(id: Types.ObjectId): Promise<T | null>
+}
---
+++ src/database/interfaces/writer.interface.ts
@@ -0,0 +1,6 @@
+import { Types } from 'mongoose'
+export interface IWriter<T> {
+	create(item: T): Promise<T>
+	update(id: Types.ObjectId, item: Partial<T>): Promise<T | null>
+	delete(id: Types.ObjectId): Promise<boolean>
+}

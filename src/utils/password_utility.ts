9b7b54259fd7133cc6794e10a00a5f11901c93b6
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-14 11:07:38 +0300

    import payloads

--- src/utils/password_utility.ts
+++ src/utils/password_utility.ts
@@ -1,41 +1,46 @@
 import bcrypt from 'bcrypt'
 import { type Request } from 'express'
 import jwt from 'jsonwebtoken'
-import { type VendorPayload } from '../dto'
+import { type VendorPayload } from '../api/dto'
 import { JWT_SECRET } from '../config'
-import { type AuthPayload } from '../dto/auth.dto'
+import { type AuthPayload } from '../api/dto/auth.dto'
 
 export const GenerateSalt = async (): Promise<string> => {
-  return await bcrypt.genSalt()
+	return await bcrypt.genSalt()
 }
 
-export const GeneratePassword = async (password: string, salt: string): Promise<string> => {
-  return await bcrypt.hash(password, salt)
+export const GeneratePassword = async (
+	password: string,
+	salt: string
+): Promise<string> => {
+	return await bcrypt.hash(password, salt)
 }
 
 export const ValidatePassword = async (
-  enteredPassword: string,
-  savedPassword: string | any,
-  salt: string | any
+	enteredPassword: string,
+	savedPassword: string | any,
+	salt: string | any
 ): Promise<boolean> => {
-  return (await GeneratePassword(enteredPassword, salt)) === savedPassword
+	return (await GeneratePassword(enteredPassword, salt)) === savedPassword
 }
 
-export const GenerateSignature = async (payload: VendorPayload): Promise<string> => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' })
+export const GenerateSignature = async (
+	payload: VendorPayload
+): Promise<string> => {
+	return jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' })
 }
 
 export const ValidateSignature = async (req: Request): Promise<boolean> => {
-  const signature = req.get('Authorization') as string
-  if (signature !== null) {
-    const payload = (jwt.verify(
-      signature.split(' ')[1],
-      JWT_SECRET
-    )) as AuthPayload
-    req.user = payload
+	const signature = req.get('Authorization') as string
+	if (signature !== null) {
+		const payload = jwt.verify(
+			signature.split(' ')[1],
+			JWT_SECRET
+		) as AuthPayload
+		req.user = payload
 
-    return true
-  }
+		return true
+	}
 
-  return false
+	return false
 }

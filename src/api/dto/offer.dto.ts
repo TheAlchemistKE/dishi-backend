970cc51a286a99f2cfcb95ce56bb447c4f6a7c78
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-15 21:56:18 +0300

    add create offer interface

--- src/api/dto/offer.dto.ts
+++ src/api/dto/offer.dto.ts
@@ -1,5 +1,22 @@
 import { object, string, number, array, date } from 'yup'
 
+export interface CreateOfferInputs {
+	offer_type: string
+	vendors: [any]
+	title: string
+	description: string
+	min_value: number
+	offer_amount: number
+	start_validity: Date
+	end_validity: Date
+	promocode: string
+	promo_type: string
+	bank: [any]
+	bins: [any]
+	pincode: string
+	is_active: boolean
+}
+
 export const OfferSchema = object({
 	offer_type: string().required(),
 	vendor: array().default([]),

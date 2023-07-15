bf0d95d6f702dd7bb995ca085f47add2cc5a757f
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-15 20:57:29 +0300

    change linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,46 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['standard-with-typescript'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  rules: {},
+	env: {
+		node: true,
+		browser: true,
+		es2021: true
+	},
+	parserOptions: {
+		sourceType: 'module'
+	},
+	plugins: ['import'],
+	extends: ['plugin:prettier/recommended'],
+	rules: {
+		'prettier/prettier': 'warn'
+	},
+	overrides: [
+		{
+			files: '*.ts',
+			parser: '@typescript-eslint/parser',
+			plugins: ['@typescript-eslint'],
+			parserOptions: {
+				project: 'tsconfig.json'
+			},
+			extends: [
+				'airbnb-typescript/base',
+				'eslint:recommended',
+				'plugin:@typescript-eslint/recommended',
+				'plugin:@typescript-eslint/eslint-recommended',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'plugin:prettier/recommended',
+				'plugin:import/recommended'
+			],
+			rules: {
+				'no-plusplus': 'off',
+				'no-underscore-dangle': 'off',
+				'@typescript-eslint/no-namespace': 'off',
+				'@typescript-eslint/no-misused-promises': 'off',
+				'@typescript-eslint/naming-convention': 'off',
+				'@typescript-eslint/no-unsafe-member-access': 'off',
+				'@typescript-eslint/no-unsafe-return': 'off',
+				'@typescript-eslint/no-unsafe-call': 'off',
+				'@typescript-eslint/no-unsafe-assignment': 'off',
+				'prettier/prettier': 'warn'
+			}
+		}
+	]
 }

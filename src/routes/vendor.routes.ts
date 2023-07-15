0b726e63561610470c06b76ecedf3331b6d17380
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-15 21:05:29 +0300

    add config for multer

--- src/routes/vendor.routes.ts
+++ src/routes/vendor.routes.ts
@@ -1,14 +1,25 @@
 import express from 'express'
+import multer from 'multer'
 import {
-  AddFood,
-  CreateVendor,
-  FetchAllVendors,
-  FetchVendorByEmail,
-  FetchVendorById
+	AddFood,
+	CreateVendor,
+	FetchAllVendors,
+	FetchVendorByEmail,
+	FetchVendorById
 } from '../api/controllers'
 import { Authenticate } from '../api/middlewares'
 
 const router = express.Router()
+const image_storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'images')
+	},
+	filename: function (req, file, cb) {
+		cb(null, new Date().toISOString() + '_' + file.originalname)
+	}
+})
+
+const images = multer({ storage: image_storage }).array('images', 10)
 
 router.post('/', CreateVendor)
 router.get('/', FetchAllVendors)

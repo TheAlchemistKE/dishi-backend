575e9b1723f8c0bd36cae465e579c782d91cbc1c
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-15 21:04:28 +0300

    export customer, location and order dtos

---
+++ src/api/dto/order.dto.ts
@@ -0,0 +1,5 @@
+export interface ProcessOrderDto {
+	status: string
+	remarks: string
+	ready_time: number
+}
--- src/api/dto/vendor.dto.ts
+++ src/api/dto/vendor.dto.ts
@@ -1,32 +1,41 @@
 export interface CreateVendorDto {
-  name: string
-  owner_name: string
-  food_type: [string]
-  pincode: string
-  address: string
-  phone: string
-  email: string
-  password: string
+	name: string
+	owner_name: string
+	food_type: [string]
+	pincode: string
+	address: string
+	phone: string
+	email: string
+	password: string
 }
-
+export interface UpdateVendorDto {
+	name?: string
+	owner_name?: string
+	food_type?: [string]
+	pincode?: string
+	address?: string
+	phone?: string
+	email?: string
+	password?: string
+}
 export interface VendorLoginDto {
-  email: string
-  password: string
+	email: string
+	password: string
 }
 
 export interface VendorPayload {
-  _id: string
-  email: string
-  address: string
-  phone: string
-  name: string
-  food_type: any
-  rating: number
+	_id: string
+	email: string
+	address: string
+	phone: string
+	name: string
+	food_type: any
+	rating: number
 }
 
 export interface UpdateVendorProfileDto {
-  name: string
-  address: string
-  phone: string
-  food_type: [string]
+	name: string
+	address: string
+	phone: string
+	food_type: [string]
 }

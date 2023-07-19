9d4dd1da34f6ee529c6d62be18d09979c75356c0
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-19 15:44:44 +0300

    add get current offers to offer repo

--- src/database/repositories/offer.repository.ts
+++ src/database/repositories/offer.repository.ts
@@ -1,8 +1,32 @@
+import { AuthPayload } from '../../api/dto'
 import { BaseRepository } from '../base'
 import { Offer, type OfferDocument } from '../models'
 
 export class OfferRepository extends BaseRepository<OfferDocument> {
 	constructor() {
 		super(Offer)
+	}
+
+	async getCurrentOffers(user: AuthPayload): Promise<any[]> {
+		const current_offer: any[] = []
+		const offers = await Offer.find().populate('vendors')
+	
+		if (offers) {
+			offers.forEach(item => {
+				if (item.vendors) {
+					item.vendors.forEach(vendor => {
+						if (vendor._id.toString() === user._id) {
+							current_offer.push(item)
+						}
+					})
+				}
+	
+				if (item.offer_type === 'GENERIC') {
+					current_offer.push(item)
+				}
+			})
+		}
+	
+		return current_offer
 	}
 }

830436f7a465d010b1100e0edffceb0dac3a7d51
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-19 15:46:35 +0300

    refactor vendor repository

--- src/database/repositories/vendor.repository.ts
+++ src/database/repositories/vendor.repository.ts
@@ -1,8 +1,34 @@
+import { Types } from 'mongoose'
 import { BaseRepository } from '../base'
 import { Vendor, type VendorDocument } from '../models'
 
 export class VendorRepository extends BaseRepository<VendorDocument> {
 	constructor() {
 		super(Vendor)
+	}
+
+	async fetchByEmail(email?: string): Promise<VendorDocument | object> {
+		try {
+			if (email !== undefined) {
+				const result = await Vendor.findOne({ email: email })
+				if (result !== null) {
+					return result
+				}
+			}
+			return {
+				message: `Vendor was not found`,
+				status: 'error'
+			}
+		} catch (error) {
+			throw new Error(String(error))
+		}
+	}
+
+	async fetchVendor(id?: Types.ObjectId, email?: string): Promise<any> {
+		if (email !== '') {
+			return this.fetchByEmail(email)
+		} else {
+			return this.findOne(id)
+		}
 	}
 }

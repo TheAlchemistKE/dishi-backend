a18c9fba1dbf1fd3c543362d6268bf252e092cb8
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-24 22:35:51 +0300

    add vendor repository

--- src/api/controllers/auth.controller.ts
+++ src/api/controllers/auth.controller.ts
@@ -1,50 +1,57 @@
 import { Request, Response, NextFunction } from 'express'
 import { type AuthPayload, type VendorLoginDto } from '../dto'
-import { FetchVendor } from './vendor.controller'
 import { GenerateSignature, ValidatePassword } from '../../utils'
+import { VendorRepository } from '../../database/repositories'
+import { Types } from 'mongoose'
+
+const repo = new VendorRepository()
 
 export const VendorLogin = async (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
-	const { email, password } = req.body as VendorLoginDto
+	try {
+		const { email, password } = req.body as VendorLoginDto
 
-	const existing_vendor = await FetchVendor('', email)
+		const existing_vendor = repo.fetchByEmail(email)
 
-	if (existing_vendor !== null) {
-		const validate = await ValidatePassword(
-			password,
-			existing_vendor.password as string,
-			existing_vendor.salt as string
-		)
+		if (existing_vendor !== null) {
+			const validate = await ValidatePassword(
+				password,
+				existing_vendor.password as string,
+				existing_vendor.salt as string
+			)
 
-		if (validate) {
-			const signature = GenerateSignature({
-				_id: String(existing_vendor._id),
-				email: String(existing_vendor.email),
-				address: String(existing_vendor.address),
-				phone: String(existing_vendor.phone),
-				name: String(existing_vendor.name),
-				food_type: existing_vendor.food_type,
-				rating: Number(existing_vendor.rating)
-			})
-			return res.json({
-				status: 'success',
-				token: String(signature)
-			})
-		} else {
-			return res.json({
-				status: 'error',
-				message: 'Invalid Password'
-			})
+			if (validate) {
+				const signature = GenerateSignature({
+					_id: String(existing_vendor._id),
+					email: String(existing_vendor.email),
+					address: String(existing_vendor.address),
+					phone: String(existing_vendor.phone),
+					name: String(existing_vendor.name),
+					food_type: existing_vendor.food_type,
+					rating: Number(existing_vendor.rating)
+				})
+				return res.json({
+					status: 'success',
+					token: String(signature)
+				})
+			} else {
+				return res.json({
+					status: 'error',
+					message: 'Invalid Password'
+				})
+			}
 		}
-	}
 
-	return res.json({
-		status: 'error',
-		message: 'Bad credentials'
-	})
+		return res.json({
+			status: 'error',
+			message: 'Bad credentials'
+		})
+	} catch (e) {
+		next(e)
+	}
 }
 
 export const VendorProfile = async (
@@ -52,18 +59,22 @@ export const VendorProfile = async (
 	res: Response,
 	next: NextFunction
 ) => {
-	const { _id } = req.user as AuthPayload
-	const vendor = await FetchVendor(_id, '')
+	try {
+		const { _id } = req.user as AuthPayload
+		const vendor = await repo.findOne(new Types.ObjectId(_id))
 
-	if (vendor !== null) {
+		if (vendor !== null) {
+			return res.json({
+				status: 'success',
+				user: vendor
+			})
+		}
+
 		return res.json({
-			status: 'success',
-			user: vendor
+			status: 'error',
+			message: 'User not found'
 		})
+	} catch (e) {
+		next(e)
 	}
-
-	return res.json({
-		status: 'error',
-		message: 'User not found'
-	})
 }
